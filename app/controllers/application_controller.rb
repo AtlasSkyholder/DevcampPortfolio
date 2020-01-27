178cbe5ae8e3242dd16493d769fae169f69a6d1b
morbi <the_morbidus@hotmail.com>
2020-01-27 16:55:00 -0500

    added DevcampViewTool module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  end
+end
+
+module DevcampViewTool
+  class Renderer
+    def self.copyright
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
